47ad02c013814eb87c8704df90f0fdad2f29ddcc
elvisthomme <elvisthomme@gmail.com>
2022-03-11 14:49:44 -0800

    Print the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase, followed by a new line
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 97 + 26; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
