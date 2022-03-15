a1268b19e23a5fbb454062a49e15e17985b03443
elvisthomme <elvisthomme@gmail.com>
2022-03-15 16:30:00 -0700

    Fix issue: incorrect output

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,28 +11,11 @@ void jack_bauer(void)
 	for (hour = 0; hour < 24; hour++)
 		for (minute = 0; minute < 60; minute++)
 		{
-			if (hour < 10)
-			{
-				_putchar(48);
-				_putchar(48 + hour);
-			}
-			else
-			{
-				_putchar(48 + (hour / 10));
-				_putchar(48 + (hour % 10));
-			}
+			_putchar(48 + (hour / 10));
+			_putchar(48 + (hour % 10));
 			_putchar(58);
-
-			if (minute < 10)
-			{
-				_putchar(48);
-				_putchar(48 + minute);
-			}
-			else
-			{
-				_putchar(48 + (hour / 10));
-				_putchar(48 + (hour % 10));
-			}
+			_putchar(48 + (hour / 10));
+			_putchar(48 + (hour % 10));
 			_putchar('\n');
 		}
 }
