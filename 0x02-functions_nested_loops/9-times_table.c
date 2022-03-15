f4cd7f264e35fc7007a8db75898110715d4426f3
elvisthomme <elvisthomme@gmail.com>
2022-03-15 16:51:31 -0700

    Print the 9 times table stating with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - print the nine times tables
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(48 + (i * j));
+			if (j == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar(' ');
+	}
+}
