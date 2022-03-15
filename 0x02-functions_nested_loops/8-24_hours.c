3bf9a5f8049f471714b5e048e69256ad8ad298a5
elvisthomme <elvisthomme@gmail.com>
2022-03-15 09:43:19 -0700

    Print every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  print every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+		for (minute = 0; minute < 60; minute++)
+		{
+			if (hour < 10)
+			{
+				_putchar(48);
+				_putchar(48 + hour);
+			}
+			else
+			{
+				_putchar(48 + ((hour - hour % 10) / 10));
+				_putchar(48 + (hour % 10));
+			}
+			_putchar(58);
+
+			if (hour < 10)
+			{
+				_putchar(48);
+				_putchar(48 + hour);
+			}
+			else
+			{
+				_putchar(48 + ((hour - hour % 10) / 10));
+				_putchar(48 + (hour % 10));
+			}
+		}
+}
