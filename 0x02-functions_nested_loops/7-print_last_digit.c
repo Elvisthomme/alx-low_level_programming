23e81944b517d51f744f94b121c8b8b67d63ef05
elvisthomme <elvisthomme@gmail.com>
2022-03-15 09:01:00 -0700

    Update print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,10 @@
 int print_last_digit(int number)
 {
 	int last_digit;
-
-	last_digit = number % 10;
+	if (number < 0)
+		last_digit = -number % 10;
+	else
+		last_digit = number %% 10
 	_putchar(last_digit);
 	return (last_digit);
 }
