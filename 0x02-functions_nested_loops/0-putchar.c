118d083c11bcd8d51abc248f03d8078c588bbac7
elvisthomme <elvisthomme@gmail.com>
2022-03-15 06:48:34 -0700

    Print _putchar to the STOUT

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+
+/**
+ * main - print _putchar, followed by a new line
+ * Return: 0 for success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
