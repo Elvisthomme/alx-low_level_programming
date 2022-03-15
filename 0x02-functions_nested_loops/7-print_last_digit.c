bddab25bd23d522340051d91501e7bb2641932f1
elvisthomme <elvisthomme@gmail.com>
2022-03-15 09:11:58 -0700

    Fix errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,11 @@
 int print_last_digit(int number)
 {
 	int last_digit;
+
 	if (number < 0)
-		last_digit = -number % 10;
+		last_digit = -(int) number % 10;
 	else
-		last_digit = number % 10;
-	_putchar(last_digit);
+		last_digit = (int) number % 10;
+	_putchar(48 + last_digit);
 	return (last_digit);
 }
