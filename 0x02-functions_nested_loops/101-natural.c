bb966f3deb2ba9f4b4e23046e5fd3afdbe688c7b
elvisthomme <elvisthomme@gmail.com>
2022-03-15 19:33:12 -0700

    Print the sum of the multiple of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - compute and print the sum of the multiple of 3 and 5
+ */
+void main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	i = 3;
+	while(i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i
+		i++;
+	}
+	printf("%d\n", sum);
+}
