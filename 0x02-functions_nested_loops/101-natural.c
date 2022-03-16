bff13393f36e8c4ab7d650b7a3898b671eba434e
elvisthomme <elvisthomme@gmail.com>
2022-03-15 19:37:56 -0700

    Change the return type of main function

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,19 +2,21 @@
 
 /**
  * main - compute and print the sum of the multiple of 3 and 5
+ * Return: always return 0
  */
-void main(void)
+int main(void)
 {
 	int i;
 	int sum;
 
 	sum = 0;
 	i = 3;
-	while(i < 1024)
+	while (i < 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i
 		i++;
 	}
 	printf("%d\n", sum);
+	return (0);
 }
