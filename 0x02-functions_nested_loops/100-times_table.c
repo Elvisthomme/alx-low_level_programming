204074e931b2a498f8a6a836ca321aa12120bed4
elvisthomme <elvisthomme@gmail.com>
2022-03-16 19:51:11 -0700

    Print n times table stating by 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the n times tables starting with 0
+ *@n: the number of times tables
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int result;
+
+	if (n < 15 && n < -1)
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				result = i * j;
+				if (n < 10 && result > 9)
+				{
+					_putchar(48 + (result / 10));
+					_putchar(48 + (result % 10));
+				}
+				else if (n < 10 && result < 10)
+				{
+					if (j != 0)
+						_putchar(' ');
+					_putchar(48 + result);
+				}
+				else if (n < 10)
+				{
+					if (j == 9)
+						continue;
+					_putchar(44);
+					_putchar(' ');
+				}
+				else
+				if (result > 99)
+				{
+					_putchar(48 + (result / 100));
+					_putchar(48 + ((result - (result / 100)) / 10));
+					_putchar(48 + (result % 10));
+				}
+				else if (result > 9)
+				{
+					_putchar(' ');
+					_putchar(48 + (result / 10));
+					_putchar(48 + (result % 10));
+				}
+				else if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(48 + result);
+				}
+				if (j == n)
+					continue;
+				_putchar(44);
+				_putchar(' ');
+			}
+		}
+	_putchar('\n');
+}
