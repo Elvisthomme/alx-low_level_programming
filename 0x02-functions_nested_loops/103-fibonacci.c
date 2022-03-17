670939a734b25d809b87e6f431402b3d764ce7ac
elvisthomme <elvisthomme@gmail.com>
2022-03-16 21:13:50 -0700

    Print the sum of the even valued term of fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,17 +9,17 @@ int main(void)
 	long int prev_prev_fib;
 	long int prev_fib = 1;
 	long int curr_fib = 1;
-	long int sum_even_fib_term;
+	int sum_even_fib_term;
 
 	curr_fib = 1;
-	while (curr_fib < 4000001)
+	while (curr_fib < 4000000)
 	{
+		if (curr_fib % 2 == 0)
+			sum_even_fib_term += curr_fib;
 		prev_prev_fib = prev_fib;
 		prev_fib = curr_fib;
 		curr_fib = prev_prev_fib + prev_fib;
-		if (curr_fib % 2 == 0)
-			sum_even_fib_term += curr_fib;
 	}
-	printf("%ld\n", sum_even_fib_term);
+	printf("%d\n", sum_even_fib_term);
 	return (0);
 }
