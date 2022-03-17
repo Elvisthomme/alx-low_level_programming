dd071e550948f68abf108843bd1a9f3e533de05b
elvisthomme <elvisthomme@gmail.com>
2022-03-16 20:25:33 -0700

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,22 @@
  */
 int main(void)
 {
-	int prev_fib;
-	int curr_fib;
+	long int prev_prev_fib;
+	long int prev_fib = 1;
+	long int curr_fib = 1;
 	int i;
 
 	i = 1;
-	curr_fib = 1;
 	while (i < 51)
 	{
 		i++;
-		printf("%d", curr_fib);
+		printf("%ld", curr_fib);
 		if (i == 50)
 			continue;
 		printf(", ");
+		prev_prev_fib = prev_fib;
 		prev_fib = curr_fib;
-		curr_fib += prev_fib;
+		curr_fib = prev_prev_fib + prev_fib;
 	}
 	printf("\n");
 	return (0);
