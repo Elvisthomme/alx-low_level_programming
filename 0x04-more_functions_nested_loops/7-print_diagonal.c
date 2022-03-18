907102f60db579d210473bf20d17420828e868d7
elvisthomme <elvisthomme@gmail.com>
2022-03-17 17:14:08 -0700

    Fixe betty style

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_line - draws a diagonal line on the terminal
+ * print_diagonal - draws a diagonal line on the terminal
  * @n: the number of times the character \ should be printed
  */
 void print_diagonal(int n)
