7d61d6a5ee08097723a3be8eeb8ba51e2a2c615c
elvisthomme <elvisthomme@gmail.com>
2022-03-21 23:21:54 +0100

    Print a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line to stdout
+ * @str: the string
+ */
+void _puts(char *str)
+{
+	int counter = 0;
+
+	while (*(s + counter))
+	{
+		_putchar(*(s + counter));
+		counter++;
+	}
+}
