b9d3dc99580be8f026e4aed7c2acabcbe1c31651
elvisthomme <elvisthomme@gmail.com>
2022-03-22 16:49:45 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
 /**
  * main - generates random valid passwords for the program 101-crackme
  * Return: always return 0
  */
 int main(void)
 {
-	printf("%ld", time(NULL));
+	printf("%ld", rand() + time(NULL));
 	return (0);
 }
