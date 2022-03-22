fb3a3b163638b296bb836cf1dc9e3e71859467a3
elvisthomme <elvisthomme@gmail.com>
2022-03-22 16:32:22 +0100

    Generate random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("%ld", time(NULL));
+	return (0);
+}
