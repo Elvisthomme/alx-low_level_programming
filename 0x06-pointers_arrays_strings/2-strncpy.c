85f796fbc76e5e6815787e926ad0639351dba043
elvisthomme <elvisthomme@gmail.com>
2022-03-23 14:09:47 +0100

    Copy n byte of the src string to the dest string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strncpy - n byte of src to dest
+ * @dest: the destination
+ * @src: the source
+ * @n: the number of byte to copy
+ * Return: a pointer to @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) && i < n-1)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	/* to include the terminating null byte*/
+	*(dest + i) = *(src + i);
+	return (dest);
+}
