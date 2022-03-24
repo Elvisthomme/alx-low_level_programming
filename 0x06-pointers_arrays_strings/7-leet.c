8aab0fa6c0e7fde7f68344c4d79527826cac24eb
elvisthomme <elvisthomme@gmail.com>
2022-03-24 17:48:06 +0100

    Update 7.leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,28 @@
 /**
- * leep - encode a string into 1337
+ * leet - encode a string into 1337
  * @str: the string to encode
  * Return: a pointer to the encoding string
  */
 char *leet(char *str)
 {
-	char *code;
 	int i = 0;
-	char c;
+	int j;
+	char char_arr[] = {'A', 'a', 'E', 'e', '0', 'o', 'l', 'L', 't', 'T'};
+	char code_arr[] = {'4', '4', '3', '3', '0', '0', '1', '1', '7', '7'};
+	char *c;
 
-	code = &c; /* just to initialize code*/
-	*(code + 'a') = '4';
-	*(code + 'A') = '4';
-	*(code + 'e') = '3';
-	*(code + 'E') = '3';
-	*(code + 'o') = '0';
-	*(code + 'O') = '0';
-	*(code + 't') = '7';
-	*(code + 'T') = '7';
-	*(code + 'l') = '1';
-	*(code + 'L') = '1';
 
 	while (*(str + i))
 	{
-		if (*(str + i ) == 'a' || *(str + i) == 'A' 
-			|| *(str + i) == 'e' || *(str + i) == 'E'
-			|| *(str + i) == 'o' || *(str + i) == 'O'
-			|| *(str + i) == 'l' || *(str + i) == 'L'
-			|| *(str + i) == 't' || *(str + i) == 'T')
+		c = str + i;
+
+		for (j = 0; j < 10; j++)
 		{
-			*(str + i) = *(code + *(str + i));
+			if (*c == char_arr[j])
+			{
+				*c = code_arr[j];
+				break;
+			}
 		}
 		i++;
 	}
