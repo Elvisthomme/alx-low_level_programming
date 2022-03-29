28130f4fc5226b9b5995d55c4ae7979ab202982d
elvisthomme <elvisthomme@gmail.com>
2022-03-29 22:28:43 +0100

    Fix betty style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,7 @@ void print_diagsums(int *a, int size)
 	int i;
 	int first_diagsums = 0;
 	int second_diagsums = 0;
-	
+
 	for (i = 0; i < size * size; i += size + 1)
 	{
 		first_diagsums += *(a + i);
