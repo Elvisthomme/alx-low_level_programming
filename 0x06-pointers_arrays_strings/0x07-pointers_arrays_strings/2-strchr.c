2b4cc27211a287832259e687f43d6cfba22a4af1
elvisthomme <elvisthomme@gmail.com>
2022-03-29 03:37:05 +0100

    Update

---
+++ 0x06-pointers_arrays_strings/0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strchr - locate a character in a string
+  * @s: the string
+  * @c: the character
+  * Return: a pointer to the first occurence of c in s or NULL
+  */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x06-pointers_arrays_strings/0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+/**
+  * _strspn - get the length of a prefix substring
+  * @s: the initial segment
+  * @accept: the accept byte
+  * @Return: the number of byte of accept in s
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int counter = 0;
+
+	while (*(s + i))
+	{
+		while (*(accept + j))
+		{
+			if (*(accept + i) == *(s + j))
+				counter++;
+			j++;
+		}
+		i++;
+	}
+	return (counter);
+}
