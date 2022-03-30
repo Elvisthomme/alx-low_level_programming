f0ea7b524cf783f8013a5d205dbd74115a4ecad0
elvisthomme <elvisthomme@gmail.com>
2022-03-30 08:35:36 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	if (!c)
-		return (NULL);
 	while (*(s + i))
 	{
 		if (*(s + i) == c)
 			return (s + i);
 		i++;
 	}
+	if (!c)
+		return (s + i);
 	return (NULL);
 }
