5aaf2a4bd2a4577a5dc280b217b0c42d213619b9
elvisthomme <elvisthomme@gmail.com>
2022-03-30 10:25:48 +0100

    Compute the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+/**
+  * _pow_recursion - compute the value of x raised to the power of y
+  * @x: the number
+  * @y: the power
+  * Return: -1 if y < 0 or x^y
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
