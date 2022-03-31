9ab778f8aa34dc950d2c4cadfa4bf115d6a823ae
elvisthomme <elvisthomme@gmail.com>
2022-04-01 00:51:49 +0100

    Fix the problem with MIN_INT

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,10 @@
 int print_last_digit(int number)
 {
 	int last_digit;
-
-	if (number < 0)
+	
+	if (number == -2147483648)
+		_putchar('0' + 8);
+	else if (number < 0)
 		last_digit = -number % 10;
 	else
 		last_digit = number % 10;
