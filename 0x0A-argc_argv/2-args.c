23acfd83fe8d4f2ba527530fbeaaa554fcd59235
elvisthomme <elvisthomme@gmail.com>
2022-04-02 04:36:13 +0100

    Print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j = 0;
+
+	while (j < argc)
+	{
+		i = 0;
+		while (*(argv[j] + i))
+		{
+			_putchar(*(argv[j] + i));
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+	return (0);
+}
