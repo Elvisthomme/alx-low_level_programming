a9bab69fcfeb9d5db79cec7f27e76bba75859e72
elvisthomme <elvisthomme@gmail.com>
2022-04-05 23:37:23 +0100

    Check for NULL arguments

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,18 +13,20 @@ char *str_concat(char *s1, char *s2)
 	int size_s1 = 0;
 	int size_s2 = 0;
 
-	while (*(s1 + size_s1))
-		size_s1++;
-	while (*(s2 + size_s2))
-		size_s2++;
+	if (s1 != NULL)
+		while (*(s1 + size_s1))
+			size_s1++;
+	if (s2 != NULL)
+		while (*(s2 + size_s2))
+			size_s2++;
 	con = malloc(sizeof(char) * (size_s1 + size_s2 + 1));
 
 	if (con == NULL)
 		return (NULL);
-	if (size_s1 > 0 && s1 != NULL)
+	if (size_s1 > 0)
 		for (; i < size_s1; i++)
 			*(con + i) = *(s1 + i);
-	if (size_s2 > 0 && s2 != NULL)
+	if (size_s2 > 0)
 		for (; i < size_s1 + size_s2; i++)
 			*(con + i) = *(s2 + i - (size_s1));
 	*(con + i) = '\0';
