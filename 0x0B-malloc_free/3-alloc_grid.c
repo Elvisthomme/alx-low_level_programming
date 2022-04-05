6b427ac0d347606384659bbcd6eb010bf392e41d
elvisthomme <elvisthomme@gmail.com>
2022-04-05 23:08:37 +0100

    Allocate memory for a grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * alloc_grid - allocate memory for a grid
+  * @width: the width of the grid
+  * @height: the height of th grid
+  * Return: a pointer to a 2 dimentional array of integers or NULL
+  */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	grid = malloc(sizeof(int *) * width);
+	for (i = 0; i <= width; i++)
+	{
+		*(grid + i) = malloc(sizeof(int) * height);
+		if (*(grid + i) == NULL)
+			return (NULL);
+		for (j = 0; j < height; j++)
+			*(*(grid + i) + j) = 0;
+	}
+	return (grid);
+}
