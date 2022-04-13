96bfaef857a4f0d6ec013c2f8bd09b1b859144f2
elvisthomme <elvisthomme@gmail.com>
2022-04-13 16:24:46 +0100

    Check for NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
   * print_name - print call the fonction @f with @name
   * @name: a string value
@@ -5,5 +7,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	*(f)(name);
+	if (name != NULL && f != NULL)
+		(*f)(name);
 }
