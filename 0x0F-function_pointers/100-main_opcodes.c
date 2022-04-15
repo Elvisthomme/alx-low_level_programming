db9e024d1a2d05a3b3975b2a195c50c260920ab2
elvisthomme <elvisthomme@gmail.com>
2022-04-15 15:57:30 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+  * print_2_last_hexa - print the to last hexa of a number
+  * @num: the number to print
+  */
+void print_2_last_hexa(unsigned int num)
+{
+	int a = 0, b = 0;
+	while (num)
+	{
+		b = a;
+		a = num - ((num / 16) * 16);
+		num /= 16;
+	}
+	printf("%x", a);
+	printf("%x", b);
+}
+
+/**
   * main - print opcode of main
   * @ac: the number of argument
   * @av: the argument vector
@@ -8,7 +25,9 @@
   */
 int main(int ac, char *av[])
 {
-	int n, i, number;
+	int n, i, number, l;
+	int *code;
+	char *c;
 	int (*m)(int, char**);
 
 	if (ac != 2)
@@ -28,11 +47,11 @@ int main(int ac, char *av[])
 		return (0);
 	}
 	m = main;
-
 	for (i = 0; i < n; i++)
 	{
-		number = atoi((char *)(*(m + i)));
-		printf("%x", number);
+		code = (int *) *(m + i + 1);
+		number = *code;
+		print_2_last_hexa(number);
 		if (i != n - 1)
 			printf(" ");
 	}
