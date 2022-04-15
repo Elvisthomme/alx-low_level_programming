01e3e99756b6186198da3870597b6b15b0910612
elvisthomme <elvisthomme@gmail.com>
2022-04-15 11:34:45 +0100

    Revert "Check for NULL"
    This reverts commit 0400eadbe90a6420390ff3a3fd2a9c63c133d201.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -67,9 +67,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(ap, int);
+<<<<<<< HEAD
 		if (num != NULL)
 			print_number(num);
 		if (i != n - 1 && separator != NULL)
+=======
+		print_number(num);
+		if (i != n - 1 && separator)
+>>>>>>> parent of 0400ead... Check for NULL
 		{
 			while (*(separator + j))
 			{
