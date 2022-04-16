bca4ecff78697f1ccb34860992099f72f5b3c25c
elvisthomme <elvisthomme@gmail.com>
2022-04-16 04:09:13 +0100

    Define a new tye struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+  * struct dog - info about a dog
+  * @name: the name of the dog
+  * @age: the age of the dog
+  * @owner: the owner of the dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
