aa10c5f387f1a38bcb7482875b817fd21bf19825
elvisthomme <elvisthomme@gmail.com>
2022-04-16 22:01:50 +0100

    Print dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+/** print_dog - print a struct dog
+  *@d: a pointer to the dog struct
+  */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name)
+			printf("Name: %s", d->name);
+		else
+			printf("Name: (nil)");
+		if (d->age)
+			printf("Age: %d", d->age);
+		else
+			printf("Name: (nil)");
+		if (d->owner)
+			printf("Name: %s", d->owner);
+		else
+			printf("Name: (nil)");
+	}
+}
