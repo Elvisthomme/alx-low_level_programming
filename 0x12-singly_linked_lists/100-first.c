b5e84bd4e10af347b0b932186491b62df5a13f5b
elvisthomme <elvisthomme@gmail.com>
2022-04-22 04:12:37 +0100

    Update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void my_startup_fun(void) __attribute__((constructor));
 void my_startup_fun(void)
 {
 printf(
-"You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n"
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
 );
 }
