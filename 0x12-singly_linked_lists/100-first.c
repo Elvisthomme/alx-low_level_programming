290b4699c9daa9f70d02575256ea51d7449c7ffc
elvisthomme <elvisthomme@gmail.com>
2022-04-22 03:56:41 +0100

    Print a message before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void my_startups_fun(void) __attribute__((constructor));
+/**
+  * my_startup_fun - print a message before the main
+  */
+void my_startup_fun(void)
+{
+printf(
+"You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n"
+);
+}
