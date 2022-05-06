b525a04d61439a0b350d71359165c06bc6494943
elvisthomme <elvisthomme@gmail.com>
2022-05-06 15:57:23 +0100

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * create_file - create a file whit a text content
+  * @filename: the name of the file
+  * @text_content
+  * Return: 1 on success, -1 on failure
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (0);
+	}
+	while (*(text_content + i))
+		i++;
+	i = write(fd, text_content, i);
+	if (i == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+	
+}
