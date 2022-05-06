1826b73641b6ea82e832507ff7807db2b56297b6
elvisthomme <elvisthomme@gmail.com>
2022-05-06 22:13:15 +0100

    Copie the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+  * main - copy a file whit the text content
+  * @argc: the number of argument
+  * @argv: the argument vector
+  * Return: 1 on success
+  */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to, i = 1;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
+	while (1)
+	{
+		i = read(fd_from, buffer, 1024);
+		if (i == -1)
+		{
+			dprintf(STDERR_FILENO,
+			"Error: Can't write to file %s\n", argv[2]);
+			exit(99);
+		}
+		i = write(fd_to, buffer, i);
+		if (i < 1024)
+			break;
+	}
+	i = close(fd_from);
+	if (i == -1)
+	{	
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(99);
+	}
+	i = close(fd_to);
+	if (i == -1)
+	{	
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(99);
+	}
+	return (1);
+}
