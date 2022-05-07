3cfd570a440e63a686391848620fcef8594733b2
elvisthomme <elvisthomme@gmail.com>
2022-05-07 10:43:28 +0100

    Fix betty style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,37 @@
 #include "main.h"
 
+
+/**
+  * copy - copy a file whit the text content
+  * @argv: the argument vector of the main
+  * @fd_from: the file descriptor source
+  * @fd_to: the destination file descriptor
+  * Return: 1 on success or the exit code for the main
+  */
+int copy(char **argv, int *fd_from, int *fd_to)
+{
+	int i = 1;
+	char buffer[1024];
+
+	while (i != 0)
+	{
+		i = read(*fd_from, buffer, 1024);
+		if (i == -1)
+		{
+			dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+			return (98);
+		}
+		i = write(*fd_to, buffer, i);
+		if (i == -1)
+		{
+			dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+			return (99);
+		}
+	}
+	return (1);
+}
 /**
   * main - copy a file whit the text content
   * @argc: the number of argument
@@ -9,7 +41,6 @@
 int main(int argc, char **argv)
 {
 	int fd_from, fd_to, i = 1;
-	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -28,25 +59,11 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while (1)
-	{
-		i = read(fd_from, buffer, 1024);
-		if (i == -1)
-		{
-			dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		i = write(fd_to, buffer, i);
-		if (i == -1)
-		{
-			dprintf(STDERR_FILENO, 
-			"Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		if (i < 1024)
-			break;
-	}
+
+	i = copy(argv, &fd_from, &fd_to);
+	if (i != 1)
+		exit(i);
+
 	i = close(fd_from);
 	if (i == -1)
 	{
